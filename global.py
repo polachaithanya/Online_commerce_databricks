e051cd2a9016757fd4e14507193fb59b60e629d0
polachaithanya <polachaithanya100@gmail.com>
2026-01-13 16:28:40 +0000

    Modified global file and removed the read code

--- global.py
+++ global.py
@@ -116,31 +116,3 @@ def read_cdm_csv(
 
     return df
 
-
-# COMMAND ----------
-
-# Paths
-cdm_path = "/Volumes/onlinecommerce/landing/purchase_volume/Parties.cdm.json"
-manifest_path = "/Volumes/onlinecommerce/landing/purchase_volume/Purchase.manifest.cdm.json"
-csv_path = "/Volumes/onlinecommerce/landing/purchase_volume/Parties/*.csv"
-
-# Build schema
-schema = read_cdm_schema(cdm_path)
-
-# Extract CSV format
-delimiter, escape, newline = extract_csv_format(
-    manifest_path,
-    "Parties"
-)
-
-# Read CSV
-df = read_cdm_csv(
-    csv_path,
-    schema,
-    delimiter,
-    escape,
-    newline
-)
-
-df.display()
-
