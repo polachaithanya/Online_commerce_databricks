6c9e9c2d311cfc6f8fb26822367e6aed0959d41b
polachaithanya <polachaithanya100@gmail.com>
2026-01-17 10:25:48 +0000

    Developing Purchase Category silver notebook

---
+++ Silver/DimPurchaseCategory_Silver.py
@@ -0,0 +1,29 @@
+# Databricks notebook source
+purchaseCategoryDf = spark.table("onlinecommerce.bronze.purchcategory")
+
+# COMMAND ----------
+
+import pyspark.sql.functions as F
+
+# COMMAND ----------
+
+dimpurchasecategory_df = purchaseCategoryDf.filter(
+    purchaseCategoryDf.RecordId.isNotNull()
+).select(
+    purchaseCategoryDf.CategoryId,
+       F.trim(purchaseCategoryDf.CategoryName).alias("CategoryName"),
+       F.when(purchaseCategoryDf.LastProcessedChange_DateTime.isNull(), "1900-01-01").otherwise(purchaseCategoryDf.LastProcessedChange_DateTime).cast("timestamp").alias("LastProcessedChange_DateTime"),
+       F.from_utc_timestamp(purchaseCategoryDf.DataLakeModified_DateTime,'CST').alias("DataLakeModified_DateTime"),
+       purchaseCategoryDf.CategoryGroupId,
+       purchaseCategoryDf.RecordId.alias("PurchCategoryRecordId"),       
+    ).withColumn("UpdatedDateTime", F.current_timestamp()
+    ).withColumn("PurchCategoryHashKey", F.xxhash64("PurchCategoryRecordId")
+    )
+display(dimpurchasecategory_df)
+
+
+
+
+# COMMAND ----------
+
+dimpurchasecategory_df.write.format("delta").mode("overwrite").saveAsTable("onlinecommerce.silver.dimpurchasecategory")
